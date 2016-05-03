17fdee279999902a598093108a88478e8426dc0e
(adm) SANCHEZ <admsanchezf@global.wfp.org>
2016-05-03 16:55:14 -0500

    Configuracion - conexion bd

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'nutrifami_blog');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'blog');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'nutrifamiBL0G');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'nutrifami.cwy5i3r1f6xk.us-east-1.rds.amazonaws.com:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
